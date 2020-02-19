b39c57355b8c01a4075870e6ea93236d13da7ef6
saproanopa <61156495+saproanopa@users.noreply.github.com>
2020-02-19 16:02:03 -0500

    Add files via upload

---
+++ piedra papel o tijera/src/piedra/papel/o/tijera/PiedraPapelOTijera.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package piedra.papel.o.tijera;
+
+/**
+ *
+ * @author 59399
+ */
+public class PiedraPapelOTijera {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        PiedraPapelTijera PiedraPapelTijera=new PiedraPapelTijera();
+        //frmAdivina.show();
+        PiedraPapelTijera.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
