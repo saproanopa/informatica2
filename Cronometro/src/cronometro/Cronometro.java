8e4f848107750cfe5a2e1bce00f5a18eefc669e3
saproanopa <61156495+saproanopa@users.noreply.github.com>
2020-02-19 15:54:48 -0500

    Add files via upload

---
+++ Cronometro/src/cronometro/Cronometro.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cronometro;
+
+/**
+ *
+ * @author 59399
+ */
+public class Cronometro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFRcronometro JFRcronometro=new JFRcronometro();
+        JFRcronometro.setVisible(true);
+    }
+    
+}
