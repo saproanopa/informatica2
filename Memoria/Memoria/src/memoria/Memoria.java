4f94b86de5d2490b66fe0a72c8c8fbb303d41230
saproanopa <61156495+saproanopa@users.noreply.github.com>
2020-03-04 00:04:29 -0500

    Add files via upload

---
+++ Memoria/Memoria/src/memoria/Memoria.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package memoria;
+
+/**
+ *
+ * @author 59399
+ */
+public class Memoria {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         JFmemoria JFmemoria=new JFmemoria();
+        //frmAdivina.show();
+        JFmemoria.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
