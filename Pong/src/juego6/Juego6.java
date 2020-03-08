32a1c5b7a645aef0a63b83a4630a1cd3983bf4e9
saproanopa <61156495+saproanopa@users.noreply.github.com>
2020-03-07 21:04:02 -0500

    Add files via upload

---
+++ Pong/src/juego6/Juego6.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package juego6;
+
+/**
+ *
+ * @author User
+ */
+public class Juego6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Pong juego = new Pong();
+        juego.setVisible(true);
+    }
+    
+}
