9387fee9a074d39ab92f195a4932484381ca14d6
saproanopa <61156495+saproanopa@users.noreply.github.com>
2020-04-26 21:31:56 -0500

    Add files via upload

---
+++ BigO/src/bigo/BigO.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bigo;
+
+import javax.swing.DefaultListModel;
+
+/**
+ *
+ * @author 59399
+ */
+public class BigO {
+    
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FrmOrdenamiento FrmOrdenamiento=new FrmOrdenamiento();
+        FrmOrdenamiento.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
