a12b5a122ce3aa609313d68c76c511eb6a3b0533
saproanopa <61156495+saproanopa@users.noreply.github.com>
2020-05-11 12:14:43 -0500

    Add files via upload

---
+++ Codigo Morse/src/codigo/morse/CodigoMorse.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codigo.morse;
+
+/**
+ *
+ * @author 59399
+ */
+public class CodigoMorse {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FrmMorse frmmorse = new FrmMorse();
+        frmmorse.setVisible(true);
+    }
+    
+}
