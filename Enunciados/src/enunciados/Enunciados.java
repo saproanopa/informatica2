6998bdfe231b8c8e1168d963b1298255a57f1a5b
saproanopa <61156495+saproanopa@users.noreply.github.com>
2020-05-11 12:24:22 -0500

    Add files via upload

---
+++ Enunciados/src/enunciados/Enunciados.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package enunciados;
+
+/**
+ *
+ * @author 59399
+ */
+
+public class Enunciados {
+
+ 
+    public static void main(String args[]) {
+        FrmEnunciados Frmenunciados = new FrmEnunciados();
+        Frmenunciados.setVisible(true);
+    }
+
+}
